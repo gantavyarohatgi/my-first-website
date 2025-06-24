e2886d44f1ccdae9f8e01ded9b0ed21895c08fcc
Gantavya <gantavya2022@gmail.com>
2025-06-24 13:03:57 +0530

    Second Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Thanks for clicking!");
+  }
+  
+  document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault(); // prevents page refresh
+    alert("Thank You for your submission.)");
+  });
+  
